afdc72d0ad3a98efb141b314451d181f9dae156a
Iryna Alchynska <68770490+ira-alchynska@users.noreply.github.com>
2021-04-07 18:58:03 +0300

    cleared app file

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,23 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FeedbackOptions.module.css';
 
-/* const FeedbackOptions = ({ options, onLeaveFeedback }) => {
- 
-    return (
-      options.map((option) => {
-      <button
-        type="button"
-        key={option}
-        name={option}
-        className={styles.button}
-        onClick={onLeaveFeedback}
-      >
-        {option}
-      </button>
-    
-      }))
-}; */
-
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
     <>
